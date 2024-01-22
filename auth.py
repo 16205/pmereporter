ec643dd8612cd7beed9e39d88959c119cfbe3f0d
16205 <gabriel.lohest@gmail.com>
2024-01-22 16:44:43 +0100

    Refactor conn_str and use update .env fct from utils

--- auth.py
+++ auth.py
@@ -1,16 +1,19 @@
 import os
 import requests
+import utils
 
 from dotenv import load_dotenv
 
 load_dotenv(override=True)
+
+connection_str = os.environ['HOST'] + 'token'
 
 def authenticate():
     headers = {
         'X-APPKEY': os.environ['APPKEY']
     }
 
-    response = requests.put(os.environ['HOST'] + 'token', headers=headers, 
+    response = requests.put(connection_str, headers=headers, 
                             data={'grant_type': 'urn:ietf:params:oauth:grant-type:jwt-bearer', 
                                 'assertion': os.environ['AUTH_TOKEN']})
 
@@ -20,7 +23,7 @@ def authenticate():
         data = response.json()
         # Using '.get()' so it returns {} if key 'access_token' does not exist
         bearer_token = data.get('access_token', {})
-        print(bearer_token)
-        return bearer_token
+        utils.update_token_env(bearer_token)
+        return
     else:
         print("Failed to retrieve data:", response.status_code)
