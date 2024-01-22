ba05b8ec1389485e47754b0c73a0176827070d9f
16205 <gabriel.lohest@gmail.com>
2024-01-22 16:48:17 +0100

    Testing what is made in other files

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-import datetime
+from datetime import datetime
 import ingest
 import auth
+import process
+import json
 
-def authenticate():
-    bearer_token = auth.authenticate()
+# auth.authenticate()
+
+# response = ingest.getEvents()
+
+dateFrom = datetime(2023, 11, 21)
+dateTo = datetime(2023, 11, 21, 23, 59, 59, 999)
+
+response = ingest.get_events(dateFrom, dateTo, [30])
+
+# print(json.dumps(response.json(), indent=4))
+
+with open('example.txt', 'w') as file:
+    file.write(json.dumps(response.json(), indent=4))
 
 def generate(date:datetime):
     pass
-
