f2e539ffbb1d641221baa7c4152e619ccab9dddc
16205 <gabriel.lohest@gmail.com>
2024-02-21 11:52:07 +0100

    update ingest function calls

--- main.py
+++ main.py
@@ -6,17 +6,16 @@ import json
 
 # auth.authenticate()
 
-# response = ingest.getEvents()
-
 dateFrom = datetime(2023, 11, 21)
 dateTo = datetime(2023, 11, 21, 23, 59, 59, 999)
 
-response = ingest.get_events(dateFrom, dateTo, [30])
+response = ingest.get_events(dateFrom, dateTo)
 
 # print(json.dumps(response.json(), indent=4))
 
 with open('example.txt', 'w') as file:
-    file.write(json.dumps(response.json(), indent=4))
+    if response.status_code == 200:
+        file.write(json.dumps(response.json(), indent=4))
 
 def generate(date:datetime):
     pass
