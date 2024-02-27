de1a713c3d8a1f98f6462f40ae289b128fa37a2c
16205 <gabriel.lohest@gmail.com>
2024-02-27 11:08:30 +0100

    Switch to table layout and add ADR headings

--- process.py
+++ process.py
@@ -2,7 +2,7 @@ import json
 from tqdm import tqdm
 from reportlab.lib.pagesizes import A4
 from reportlab.lib import colors
-from reportlab.platypus import SimpleDocTemplate, Paragraph, Table, TableStyle, Frame, Spacer
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Table, TableStyle, Frame, Spacer, PageBreak
 from reportlab.lib.styles import getSampleStyleSheet
 from datetime import datetime
 
@@ -13,20 +13,23 @@ def get_resources_from_departments(departments):
 
     return keys
 
-def generate_pdfs(missions):
+def add_mission_location_intervention():
+    pass
+
+def generate_pdfs(missions, locations, sources):
 
     # Parse JSON data
     for mission in tqdm(missions["items"]):
         resources = {}
         customers = {}
         for resource in mission["resources"]:
-            if not resource['label'].startswith("RG -") and not resource['label'].startswith("BUNKER "):
+            if not resource['label'].startswith("RG -") and not resource['label'].startswith("BUNKER ") and not resource['label'].startswith("Vincotte"):
                 resources[f"{resource['key']}"] = {'name': resource['label'], 'phone': resource['mobile']}
         for customer in mission["customers"]:
             customers[f"{customer['key']}"] = {'name': customer['label'], 'phone': customer['phone']}
         
         # Create a PDF document
-        doc = SimpleDocTemplate(f".\generated\{mission['key']}.pdf", pagesize=A4)
+        doc = SimpleDocTemplate(f".\generated\{mission['key']}.pdf", pagesize=A4, leftmargin=50, topMargin=100)
 
         # Define styles
         styles = getSampleStyleSheet()
@@ -34,94 +37,83 @@ def generate_pdfs(missions):
         # Create content elements
         elements = []
 
-        # Add a spacer of 25 points height (adjust as needed)
-        elements.append(Spacer(1, 25))
-
-        # # Title
-        # canvas.saveState()
-        # title = "Vinçotte - Mission order"
-        # canvas.setFont("Helvetica", 15)
-        # text_width = canvas.stringWidth(title, "Helvetica", 15)
-        # canvas.drawString((doc.pagesize[0] - text_width) / 2, 800, title)
-
-        """# Add logos
-        logo1 = Image(logo1_path, width=60, height=60)
-        logo2 = Image(logo2_path, width=207.6, height=100)
-        elements.append(logo1)
-        elements.append(logo2)"""
-
         # Add content
+
+        # Title
+        elements.append(Paragraph(f"Mission order n° {mission['key']} - Vinçotte", styles['Title']))
+        elements.append(Spacer(1, 10))
+
+        # Mission details heading -------------------------------------
+        elements.append(Paragraph(f"Mission details", styles['Heading2']))
                 
+        # Table data
+        mission_table_data = []
+        
         # Parse the datetime string into a datetime object
         datetime_start = datetime.strptime(mission['start'], "%Y-%m-%dT%H:%M:%S")
         datetime_end = datetime.strptime(mission['end'], "%Y-%m-%dT%H:%M:%S")
-
-        # Date
-        elements.append(Paragraph(f"<b>Date of intervention:</b> {datetime_start.strftime('%d/%m/%Y')}"))
-        elements.append(Spacer(1, 10))
+            
+        # Date and time
+        mission_table_data.append([Paragraph("<b>Date of intervention</b>"), Paragraph(f"{datetime_start.strftime('%d/%m/%Y')}")])
+        mission_table_data.append([Paragraph("<b>Start time</b>"), Paragraph(f"{datetime_start.strftime('%H:%M')}")])
+        mission_table_data.append([Paragraph("<b>End time</b>"), Paragraph(f"{datetime_end.strftime('%H:%M')}")])
         
-        # Start & end times
-        elements.append(Paragraph(f"<b>Start time:</b> {datetime_start.strftime('%H:%M')}"))
-        elements.append(Paragraph(f"<b>End time:</b> {datetime_end.strftime('%H:%M')}"))
-        elements.append(Spacer(1, 10))
-
         # Agents
         for index, item in enumerate(resources):
-            elements.append(Paragraph(f"<b>Agent {index+1}:</b> {resources[item]['name']}"))
-            elements.append(Paragraph(f"<b>Phone:</b> {resources[item]['phone']}"))
-            elements.append(Spacer(1, 10))
-        
+            agent_label = "<b>Agent</b><br/><br/>" if len(resources) == 1 else f"<b>Agent {index+1}</b><br/><br/>"
+            agent_phone_label = "<b>Phone</b>"
+            mission_table_data.append([Paragraph(agent_label+agent_phone_label), 
+                                       Paragraph(f"{resources[item]['name']}<br/><br/>{resources[item]['phone']}")])
+            
+
         # Clients
         for index, item in enumerate(customers):
-            elements.append(Paragraph(f"<b>Client:</b> {customers[item]['name']}"))    
-            elements.append(Paragraph(f"<b>Phone:</b> {customers[item]['phone']}"))
-            elements.append(Spacer(1, 10))   
-        
+            client_label = "<b>Client</b><br/><br/>" if len(customers) == 1 else f"<b>Client {index+1}</b><br/><br/>"    
+            client_phone_label = "<b>Phone</b>"
+            mission_table_data.append([Paragraph(client_label+client_phone_label), 
+                                       Paragraph(f"{customers[item]['name']}<br/><br/>{customers[item]['phone']}")])
+
         # Location
-        elements.append(Paragraph(f"<b>Intervention location:</b> "))
-        elements.append(Spacer(1, 10))
+        mission_table_data.append([Paragraph("<b>Intervention location</b>"), Paragraph(f"")])
 
         # Departure location
         if mission['fields']['DEPARTUREPLACE'] is not None:
-            elements.append(Paragraph(f"<b>Departure location:</b> {mission['fields']['DEPARTUREPLACE']}"))
-            elements.append(Spacer(1, 10))
+            mission_table_data.append([Paragraph("<b>Departure location</b>"), Paragraph(f"{mission['fields']['DEPARTUREPLACE']}")])
 
         # Remarks
         if mission['remark'] is not None:
-            elements.append(Paragraph(f"<b>Remarks:</b> {mission['remark']}"))
-
-        frame = Frame(50, 50, 400, 600, id='my_frame')
-        
-        # Table data
-        data = [
-            ['Header 1', 'Header 2', 'Header 3'],
-            ['Data 1', 'Data 2', 'Data 3'],
-            ['Data 4', 'Data 5', 'Data 6'],
-            ['Data 7', 'Data 8', 'Data 9']
-        ]
+            mission_table_data.append([Paragraph("<b>Comments/Remarks</b>"), Paragraph(f"{mission['remark']}")])
 
         # Create the table with the data
-        table = Table(data)
+        mission_table = Table(mission_table_data, colWidths=[111, None])
 
-        # Define a TableStyle with invisible borders
+        # Define a TableStyle
         style = TableStyle([
             ('TEXTCOLOR', (0,0), (-1,0), colors.black),
             ('ALIGN', (0,0), (-1,-1), 'LEFT'),
             ('VALIGN', (0,0), (-1,-1), 'TOP'),
-            ('FONTNAME', (0,0), (-1,0), 'Helvetica-Bold'),
-            ('BOTTOMPADDING', (0,0), (-1,-1), 10),
-            ('TOPPADDING', (0,0), (-1,-1), 10),
-            # Set border color to transparent to hide the table borders
-            ('LINEABOVE', (0,0), (-1,-1), 0, colors.transparent),
-            ('LINEBELOW', (0,0), (-1,-1), 0, colors.transparent),
-            ('LINEBEFORE', (0,0), (-1,-1), 0, colors.transparent),
-            ('LINEAFTER', (0,0), (-1,-1), 0, colors.transparent),
+            ('FONTNAME', (0,0), (-1,0), 'Helvetica'),
+            ('BOTTOMPADDING', (0,0), (-1,-1), 5),
+            ('TOPPADDING', (0,0), (-1,-1), 5),
+            ('GRID', (0,0), (-1,-1), 1, colors.black),
         ])
 
         # Apply the style to the table
-        table.setStyle(style)
+        mission_table.setStyle(style)
 
-        # frame.add(table)
+        # Add table to list of flowables
+        elements.append(mission_table)
+
+        if sources is None:
+            # ADR Information Heading -------------------------------------
+            elements.append(PageBreak())
+            elements.append(Paragraph("<b>ADR Information</b>", styles['Heading2']))
+
+            # Description
+            elements.append(Paragraph("<b>Marchandises ADR transportées:<br/>Getransporteerde ADR stoffen:</b>", styles['Heading3']))
+            
+            # UN number & description
+            elements.append(Paragraph(f""))
 
         # Build the PDF document
         doc.build(elements, onFirstPage=add_header_footer, onLaterPages=add_header_footer)
@@ -134,13 +126,13 @@ def add_header_footer(canvas, doc):
     # Footer
     footer_text = "Page %d" % doc.page
     canvas.setFont("Helvetica", 10)
-    canvas.drawString(40, 10, footer_text)  # Adjust coordinates as needed
+    canvas.drawString(75, 20, footer_text)  # Adjust coordinates as needed
 
     # Logo image to include
     logo1_path = "media\Vincotte_RGB_H.png"
     logo2_path = "media\Member-Group-Kiwa-FC.jpg"
     canvas.drawImage(logo1_path, 75, 759, width=52, height=52)
-    canvas.drawImage(logo2_path, 135, 760, width=103.8, height=50)
+    canvas.drawImage(logo2_path, 135, 760, width=62.28, height=30)
     
     # Optionally add more elements here (e.g., footer text)
     
