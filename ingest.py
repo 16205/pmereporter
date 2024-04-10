9a8282ef60591f561aea37f1c53f58ccc35d2a19
16205 <gabriel.lohest@gmail.com>
2024-04-10 14:50:01 +0200

    Add status messages

--- ingest.py
+++ ingest.py
@@ -2,6 +2,7 @@ import os
 import requests
 import utils
 import sys
+from tqdm import tqdm
 from datetime import datetime
 
 from dotenv import load_dotenv
@@ -16,8 +17,9 @@ headers = {
         'Content-Type': 'application/json'
     }
 
-def get_events(dateFrom:datetime=None, dateTo:datetime=None, departments:list=None, resources:list=None, 
+def get_events(dateFrom:datetime, dateTo:datetime=None, departments:list=None, resources:list=None, 
                categories:list=[52,60,63,65,78,80]):
+    
     dateFrom, dateTo = utils.format_date(dateFrom), utils.format_date(dateTo)
     
     json = {
@@ -37,16 +39,21 @@ def get_events(dateFrom:datetime=None, dateTo:datetime=None, departments:list=No
     # if categories != None:
     #     json['categories'] = categories
 
+    print("Getting mission events...")
     # TODO: handle error if bad bearer token and other errors
     response = requests.post(connection_str + 'do/list', headers=headers, json=json)
 
     if response.status_code == 200:
-        return response
+        # Return response content only (this is why .json() is used)
+        print(f"Got {len(response.json()['items'])} mission events!")
+        return response.json()
     
     elif response.status_code == 401:
-        sys.exit(f"Please authenticate before requesting for data. {response.status_code} Unauthorized.")
+        sys.exit(f"Please authenticate to PlanningPME API before requesting for data. {response.status_code} Unauthorized.")
     else:
         sys.exit(f"Failed to retrieve data: {response.status_code}")
+
+
 
 def get_departments(id:int=None):
     
@@ -66,6 +73,3 @@ def get_categories():
     response = requests.get(connection_str + 'category', headers=headers)
 
     return response
-
-def get_locations():
-    pass
