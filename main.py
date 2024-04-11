b5bee092da80f1a2724f7d2448915f4b0e183d31
16205 <gabriel.lohest@gmail.com>
2024-04-11 08:40:55 +0200

    Testing

--- main.py
+++ main.py
@@ -9,29 +9,38 @@ import json
 dateFrom = datetime(2023, 11, 21)
 dateTo = datetime(2023, 11, 21, 23, 59, 59, 999)
 
-# response = ingest.get_events(dateFrom, dateTo)
-
-# print(json.dumps(response.json(), indent=4))
-
-# # write missions to file
-# with open('example.txt', 'w') as file:
-#     if missions.status_code == 200:
-#         file.write(json.dumps(response.json(), indent=4))
-
-def generate(date:datetime=None):
-    # Call ingest.get_events()                      (PPME       in  )
+def fetch_and_store(date:datetime=None):
+    # Call ingest.get_events()                      (PPME       in )
     missions = ingest.get_events(dateFrom, dateTo, departments=[30])
 
-    # Call ingest.get_locations()                   (PPME       in  )
+    # Call ingest.get_locations()                   (PPME       in )
     missions = ingest.get_locations(missions)
 
-    # Call ingest.get_sources()                     (SharePoint in  )
-    # ingest.get_sources()
+    # write missions to file
+    with open('temp/missions.json', 'w') as file:
+        json.dump(missions, file)
 
-    # Feed results into process.generate_pdfs()     (Internal       )
-    process.generate_pdfs(missions)
-    # Call ???outbound???.send_mission_orders()     (Outlook    out )
-    pass
+    # Call ingest.get_sources()                     (SharePoint in )
+    sources = ingest.get_sources()
+
+    # write sources to file
+    with open('temp/sources.json', 'w') as file:
+        json.dump(sources, file)
+
+    # Feed results into process.generate_pdfs()     (Internal      )
+    # process.generate_pdfs(missions, sources)
+    # Call outbound.send_mission_orders()           (Outlook    out)
+
+def generate(date:datetime=None):
+    with open('temp/missions.json', 'r') as file1, \
+         open('temp/sources.json', 'r') as file2:
+        missions = file1.read()
+        sources = file2.read()
+
+    # Feed results into process.generate_pdfs()     (Internal      )
+    process.generate_pdfs(missions, sources)
+    # Call outbound.send_mission_orders()           (Outlook    out)
+    
 
 def check_mat_double_booking():
     # Call ingest.get_events()
@@ -39,4 +48,5 @@ def check_mat_double_booking():
     # Feed results into process.check_conflicts
     pass
 
+# fetch_and_store()
 generate()
