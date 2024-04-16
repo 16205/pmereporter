0d8068e0c6ff9620bff88c379847c510e6f2eb32
16205 <gabriel.lohest@gmail.com>
2024-04-16 18:35:53 +0200

    Add functions to handle and proccess text (mainly for mission comments)

--- utils.py
+++ utils.py
@@ -1,4 +1,8 @@
+import regex
+import re
 from datetime import datetime, timedelta
+from reportlab.platypus import Paragraph
+import json
 
 def update_token_env(new_token:str):
     """
@@ -12,7 +16,7 @@ def update_token_env(new_token:str):
     - new_token: The new token value to be updated in the .env file.
 
     Returns:
-    None
+    - None
     """
     env_file_path = '.env'
     token_key = 'BEARER_TOKEN'
@@ -77,3 +81,112 @@ def timedelta_to_days_float(td:timedelta):
     """
     days_float = td.days + (td.seconds/86400)
     return days_float
+
+def format_text(api_text):
+    """
+    Formats the given text to be web-friendly by converting Unicode escape sequences to actual Unicode characters,
+    replacing Windows line endings with HTML line breaks, and removing redundant HTML line breaks and whitespace.
+
+    This function takes a string that may contain Unicode escape sequences and Windows line endings (\r\n),
+    and formats it for better web display. It converts Unicode escape sequences to their corresponding characters,
+    replaces Windows line endings with HTML line breaks (<br/>), removes redundant HTML line breaks, and trims
+    leading and trailing whitespace and newlines.
+
+    Parameters:
+    - api_text (str): The text to be formatted, potentially containing Unicode escape sequences and Windows line endings.
+
+    Returns:
+    - str: The formatted text, with Unicode characters properly displayed, Windows line endings replaced by HTML line breaks,
+           redundant line breaks and whitespace removed, and no <br/> tags at the beginning or end.
+    """
+    # Convert Unicode sequences to actual Unicode characters
+    api_text = api_text.encode('utf8').decode('utf8')
+    
+    # Replace Windows line endings (\r\n) with HTML line breaks(<br/>)
+    formatted_text = api_text.replace('\r\n', '<br/>')
+
+    # Remove redundant HTML line breaks
+    formatted_text = re.sub(r'(<br/>){3,}', '<br/>', formatted_text)
+    
+    # Strip leading and trailing whitespace and newlines
+    formatted_text = formatted_text.strip()
+    
+    # Remove <br/> tags from the beginning and end of the text
+    formatted_text = re.sub(r'^<br/>|<br/>$', '', formatted_text)
+    
+    # Strip leading and trailing whitespace and newlines
+    formatted_text = formatted_text.strip()
+
+    # Remove <br/> tags from the beginning and end of the text
+    formatted_text = re.sub(r'^<br/>|<br/>$', '', formatted_text)
+    
+    # Strip leading and trailing whitespace and newlines
+    formatted_text = formatted_text.strip()
+
+    # Remove <br/> tags from the beginning and end of the text
+    formatted_text = re.sub(r'^<br/>|<br/>$', '', formatted_text)
+            
+    return formatted_text
+
+def calculate_paragraph_height(text, width, style):
+    """
+    Calculate the height of a Paragraph given text, width, and style.
+    
+    :param text: The text content of the paragraph.
+    :param width: The width constraint of the paragraph.
+    :param style: The style to apply to the paragraph, which includes font size and leading.
+    :return: Height of the paragraph in points.
+    """
+    # Create a Paragraph object
+    para = Paragraph(text, style)
+    
+    # Use wrap method to determine the space required by the text
+    # wrap returns a tuple (actual_used_width, height_needed)
+    _, height = para.wrap(width, 10000)  # Large height to avoid premature wrapping
+    
+    return height
+
+def ajust_paragraph_height(text, max_height, width, style):
+    # TODO: modify this function to make it iterative so that the 2nd paragraph gets split (if its height is 
+    # superior to the max height) as well into a 3rd paragraph and so on
+
+    # Initialize the parts
+    # Split the text into sentences
+    part1 = split_into_sentences(text)  # Start with all sentences in part1
+    part2 = ""
+
+    # Set while condition to true at first iteration
+    part1_height = max_height + 1
+
+    # Iterate from the end, moving sentences from part1 to part2
+    while part1_height > max_height:
+        part2 = part1.pop() + " " + part2 # Move the last sentence to part2
+        part1_height = calculate_paragraph_height(" ".join(part1).strip(), width, style) # Re evaluate the height of part1
+    
+    # Ensure no leading or trailing spaces
+    part1 = " ".join(part1).strip()
+
+    # Combine the two parts
+    result = []
+    result.append(part1)
+    result.append(part2)
+
+    return result
+
+def save_to_txt(missions):
+    # write missions to txt file with indentation for dev purpose
+    with open('example.txt', 'w') as file:
+        file.write(json.dumps(missions, indent=4))
+
+def split_into_sentences(text):
+    # Create a pattern that matches periods, exclamation marks, or question marks
+    # that are not preceded by any of the specified abbreviations.
+    pattern = r'(?<!\b(Mr|Mrs|Mme|M)\b\.)(?<=[.!?])\s+(?=[A-Z])'
+
+    # Use regex to split the text based on the pattern
+    sentences = regex.split(pattern, text)
+
+    # Remove None elements from the list
+    sentences = [s for s in sentences if s]
+
+    return sentences
