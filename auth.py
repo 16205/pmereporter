7ed5764dcbd9c7fad9e4c354878a317449d3036a
16205 <gabriel.lohest@gmail.com>
2024-04-21 18:56:56 +0200

    Add Microsoft authentication to send emails

--- auth.py
+++ auth.py
@@ -1,13 +1,17 @@
 from dotenv import load_dotenv
-
+from exchangelib import Account, OAuth2Credentials, Configuration, Identity
+from msal import ConfidentialClientApplication
+from office365.runtime.auth.authentication_context import AuthenticationContext
 from office365.runtime.auth.client_credential import ClientCredential
 from office365.sharepoint.client_context import ClientContext
-
+import http.server
 import os
 import requests
-
+import socketserver
+import threading
+import urllib
 import utils
-
+import webbrowser
 
 def authenticate_to_ppme():
     """
@@ -68,3 +72,97 @@ def authenticate_to_shpt():
     ctx = ClientContext(site_url).with_credentials(client_credential)
 
     return ctx
+
+def authenticate_to_ms_graph():
+    """
+    Authenticates to Microsoft Graph using the OAuth 2.0 client credentials flow.
+
+    This function retrieves the client ID and client secret from the environment
+    variables and creates a confidential client application using the MSAL library.
+    It then uses the authorization code to obtain an access token and refresh token
+    from the Microsoft identity platform. The access token is stored in the
+    MS_ACCESS_TOKEN environment variable and the refresh token is stored in the
+    MS_REFRESH_TOKEN environment variable.
+
+    Returns:
+        str: The access token obtained from the Microsoft identity platform.
+    """
+    load_dotenv(override=True)
+    # Constants
+    CLIENT_ID = os.environ['MS_CLIENT_ID']
+    CLIENT_SECRET = os.environ['MS_CLIENT_SECRET']
+    TENANT_ID = os.environ['MS_TENANT_ID']
+    AUTHORITY_URL = f'https://login.microsoftonline.com/{TENANT_ID}'
+    SCOPE = ['Mail.Send', 'User.Read']
+
+    # Initialize MSAL ConfidentialClientApplication
+    app = ConfidentialClientApplication(CLIENT_ID, authority=AUTHORITY_URL, client_credential=CLIENT_SECRET)
+
+    # Function to handle the OAuth callback and extract the authorization code
+    class Handler(http.server.SimpleHTTPRequestHandler):
+        def do_GET(self):
+            global authorization_code
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            query = urllib.parse.urlparse(self.path).query
+            code = urllib.parse.parse_qs(query).get('code')
+            if code:
+                authorization_code = code[0]
+                self.wfile.write(b"Authentication successful. You can now close this window.")
+                # Signal the server to shutdown
+                threading.Thread(target=httpd.shutdown).start()  # Use a thread to call shutdown
+            else:
+                self.wfile.write(b"Failed to authenticate.")
+                authorization_code = None
+
+    # Start temporary web server on a separate thread
+    def run_server():
+        global httpd
+        with socketserver.TCPServer(("", 8000), Handler) as httpd:
+            httpd.serve_forever()
+
+    server_thread = threading.Thread(target=run_server)
+    server_thread.start()
+
+    auth_url = app.get_authorization_request_url(SCOPE)
+    webbrowser.open(auth_url)
+
+    # Wait for the server to handle the request
+    server_thread.join()
+
+    # Check if we received an authorization code
+    if authorization_code:
+        result = app.acquire_token_by_authorization_code(authorization_code, scopes=SCOPE)
+        if result and 'access_token' in result and 'refresh_token' in result:
+            utils.update_token_env(result['access_token'], 'MS_ACCESS_TOKEN')
+            utils.update_token_env(result['refresh_token'], 'MS_REFRESH_TOKEN')
+            print('Token acquired')
+            return result['access_token']
+        else:
+            print('Failed to acquire token:', result.get('error_description'))
+    else:
+        print('No authorization code was received.')
+
+def refresh_access_token():
+    CLIENT_ID = os.environ['MS_CLIENT_ID']
+    CLIENT_SECRET = os.environ['MS_CLIENT_SECRET']
+    TENANT_ID = os.environ['MS_TENANT_ID']
+    refresh_token = os.environ['MS_REFRESH_TOKEN']
+    token_url = f"https://login.microsoftonline.com/{TENANT_ID}/oauth2/v2.0/token"
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    body = {
+        "grant_type": "refresh_token",
+        "refresh_token": refresh_token,
+        "client_id": CLIENT_ID,
+        "client_secret": CLIENT_SECRET,
+        "scope": "https://graph.microsoft.com/.default offline_access"
+    }
+    response = requests.post(token_url, headers=headers, data=body)
+    response.raise_for_status()
+    new_tokens = response.json()
+    utils.update_token_env(new_tokens.get('access_token'), 'MS_ACCESS_TOKEN')
+    utils.update_token_env(new_tokens.get('refresh_token'), 'MS_REFRESH_TOKEN')
+    return new_tokens['access_token']
