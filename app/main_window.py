a922b61bdd0f17e667f009ea707061ece65772c1
16205 <gabriel.lohest@gmail.com>
2024-05-13 14:41:57 +0200

    Refactor tables setups and add sent elements table

--- app/main_window.py
+++ app/main_window.py
@@ -9,23 +9,28 @@ import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from ui.ui_main_window import Ui_MainWindow
 import main
+from modules import utils
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
         self.setupUi(self)
         self.showMaximized()  # This line maximizes the window on startup
-        self.setupTable() # Initialize the model for tableView
+        self.setupMissionTable() # Initialize the model for Mission tableView
+        self.setupSentTable() # Initialize the model for Sent Elements tableView
         self.current_task = None  # Add a variable to track the current task
         self.message = None # The message to display when the task is ongoing
         self.conflict_colors = {}  # Initialize the dictionary to store colors for each source
         self.color_index = 0  # Initialize the index for assigning colors
+
+        utils.init_folders()
 
         # Connect buttons to functions
         self.fetchButton.clicked.connect(self.fetch_data)
         self.genButton.clicked.connect(self.generate_mission_orders)
         self.sendButton.clicked.connect(self.send_mission_orders)
         self.checkSources.clicked.connect(self.check_source_conflicts)
+        self.syncButton.clicked.connect(self.sync_sent_elements)
 
     def exception_hook(exctype, value, traceback):
         QtWidgets.QMessageBox.critical(None, "Error", str(value))
@@ -53,32 +58,63 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             raise Exception
             # print(f"Error deleting temporary files: {e}")
 
-    def setupTable(self):
+    def apply_styleSheet(self, table):
+        # Add padding for text in cells
+        table.setStyleSheet("""
+            QTableView::item {
+                padding: 5px;  /* Adjust padding as needed */
+            }
+            QTableView::item:selected {
+                background-color: #bcdcf4;  /* Explicitly set the background color for selected items */
+                color: black;  /* Ensure the text color is something visible against the background */
+            }
+        """)
+
+    def setupMissionTable(self):
         # Initialize the model for tableView
-        self.model = QStandardItemModel(self)
-        self.missionTableView.setModel(self.model)
+        self.missionModel = QStandardItemModel(self)
+        self.missionTableView.setModel(self.missionModel)
 
         # Set the table view to only allow checkbox changes
         self.missionTableView.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)  # Disable text editing
         self.missionTableView.setSelectionBehavior(QtWidgets.QAbstractItemView.SelectRows)  # Enable row selection
 
         # Set column headers
-        self.headers = ['Select', 'Agents', 'Date & time', 'Customer', 'SO n°', 'Intervention n°', 'Departure From', 'Location', 'RT Sources']
-        self.model.setHorizontalHeaderLabels(self.headers)
+        self.missionHeaders = ['Select', 'Agents', 'Date & time', 'Customer', 'SO n°', 'Intervention n°', 'Departure From', 'Location', 'RT Sources']
+        self.missionModel.setHorizontalHeaderLabels(self.missionHeaders)
+
+        self.apply_styleSheet(self.missionTableView)
+
+        self.missionTableView.verticalHeader().setDefaultAlignment(Qt.AlignCenter)
+    def setupSentTable(self):
+        # Initialize the model for tableView
+        self.sentModel = QStandardItemModel(self)
+        self.sentElementsTableView.setModel(self.sentModel)
+
+        # Set the table view to only allow checkbox changes
+        self.sentElementsTableView.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)  # Disable text editing
+        self.sentElementsTableView.setSelectionBehavior(QtWidgets.QAbstractItemView.SelectRows)  # Enable row selection
+
+        self.sentHeaders = ['Recipients', 'Subject', 'Sent Time']
+        self.sentModel.setHorizontalHeaderLabels(self.sentHeaders)
+
+        self.apply_styleSheet(self.sentElementsTableView)
+
+        self.sentElementsTableView.verticalHeader().hide()
 
     # ------------------ Functions that interact with the GUI ------------------
 
-    def load_data_to_table(self, file_path, conflicts=None):
+    def load_data_to_mission_table(self, file_path, conflicts=None):
         # Clear existing data from the model
-        self.model.clear()
+        self.missionModel.clear()
 
         # Load JSON data from a file
         with open(file_path, 'r') as file:
             data = json.load(file)
 
         # Set column headers
-            headers = self.headers
-            self.model.setHorizontalHeaderLabels(headers) 
+        headers = self.missionHeaders
+        self.missionModel.setHorizontalHeaderLabels(headers) 
 
         # Assuming JSON data is a list of dictionaries
         if data:
@@ -108,30 +144,30 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
                 # Set vertical alignment for all items in the row
                 for cell in row:
                     cell.setTextAlignment(Qt.AlignVCenter | Qt.AlignLeft)
-                self.model.appendRow(row)
-                
+                self.missionModel.appendRow(row)
+
             # Resize columns and rows
             self.missionTableView.resizeColumnsToContents()
-            for row in range(self.model.rowCount()):
-                self.missionTableView.setRowHeight(row, 50)
+            for row in range(self.missionModel.rowCount()):
+                self.missionTableView.setRowHeight(row, 60)
 
-        # Load JSON data from a file and populate the table...
+        # Highlight rows containing conflicts...
         if conflicts:
             self.highlight_conflict_rows(conflicts)
 
 
     def get_selected_items(self):
         selected_items = []
-        for row in range(self.model.rowCount()):
-            if self.model.item(row, 0).checkState() == Qt.Checked:
-                mission_key = self.model.item(row, 5).text()  # Assuming the mission key is in the sixth column
+        for row in range(self.missionModel.rowCount()):
+            if self.missionModel.item(row, 0).checkState() == Qt.Checked:
+                mission_key = self.missionModel.item(row, 5).text()  # Assuming the mission key is in the sixth column
                 selected_items.append(mission_key)
         return selected_items
 
     def show_conflict_results(self, result_info):
         if result_info['data']:
             # There are conflicts, show the detailed message and update the table
-            self.load_data_to_table("./temp/missions.json", result_info['data'])
+            self.load_data_to_mission_table("./temp/missions.json", result_info['data'])
             QtWidgets.QMessageBox.information(self, "Conflict Results", result_info['message'])
         else:
             # No conflicts, just show the message
@@ -148,12 +184,49 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def highlight_conflict_rows(self, conflicts):
         # Call this method after data is loaded into the table
         self.assign_colors_to_conflicts(conflicts)
-        for row in range(self.model.rowCount()):
-            item_key = self.model.item(row, 5).text()  # Assuming key is in the sixth column
+        for row in range(self.missionModel.rowCount()):
+            item_key = self.missionModel.item(row, 5).text()  # Assuming key is in the sixth column
             for source, missions in conflicts.items():
                 if item_key in missions:
-                    for col in range(self.model.columnCount()):
-                        self.model.item(row, col).setBackground(QColor(self.conflict_colors[source]))
+                    color = QColor(self.conflict_colors[source])
+                    for col in range(self.missionModel.columnCount()):
+                        item = self.missionModel.item(row, col)
+                        item.setBackground(color)
+                        # Set style for each item individually
+                        # item.setData('background-color: %s;' % color.name())
+
+    def load_data_to_sent_table(self, file_path):
+        self.sentModel.clear()
+
+        # Load JSON data from a file
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+
+        headers = self.sentHeaders
+        self.sentModel.setHorizontalHeaderLabels(headers)
+
+        # Assuming JSON data is a list of dictionaries
+        if data:
+            # Populate the rows of the table
+            for item in data:
+                row = []
+                recipients = "\n".join(f"{recipient}" for recipient in item.get('recipients').split(', '))
+                # Append other data
+                row.extend([
+                    QStandardItem(recipients),
+                    QStandardItem(item.get('subject')),
+                    QStandardItem(item.get('sent_time'))
+                ])
+                # Set vertical alignment for all items in the row
+                for cell in row:
+                    cell.setTextAlignment(Qt.AlignVCenter | Qt.AlignLeft)
+                self.sentModel.appendRow(row)
+                
+            # Resize columns and rows
+            for row in range(self.sentModel.rowCount()):
+                self.sentElementsTableView.setRowHeight(row, 60)
+            for column in range(self.sentModel.columnCount()):
+                self.sentElementsTableView.setColumnWidth(column, 400)
 
     # ------------------ Functions related to the worker call ------------------
 
@@ -180,9 +253,9 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def task_finished(self):
         # Check which task finished and act accordingly
         if self.current_task == 'fetch_and_store':
-            self.load_data_to_table("./temp/missions.json")
-        # elif self.current_task == 'generate':
-        #     pass
+            self.load_data_to_mission_table("./temp/missions.json")
+        elif self.current_task == 'sync_sent_elements':
+            self.load_data_to_sent_table("./temp/sentElements.json")
         self.progress_dialog.setValue(100)  # Update progress dialog to show completion
         self.current_task = None  # Reset current task
         self.message = None # Clear message
@@ -223,7 +296,9 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.start_thread(self.current_task, self.message)
 
     def sync_sent_elements(self):
-        pass
+        self.current_task = 'sync_sent_elements'
+        self.message = 'Syncing sent elements'
+        self.start_thread(self.current_task, self.message)
 
 # ------------------ Worker object ------------------
 
@@ -253,6 +328,8 @@ class Worker(QThread):
                 self.result_ready.emit({'message': message, 'data': result})
             else:
                 self.result_ready.emit({'message': "No conflicts detected!", 'data': None})
+        elif self.task_type =='sync_sent_elements':
+            main.get_sent_elements(*self.args, **self.kwargs)
         self.finished.emit()
 
     def handle_progress(self, progress):
