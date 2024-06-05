ba9c31b0d70c26bb25510c43811d44520929fe03
16205 <gabriel.lohest@gmail.com>
2024-06-05 14:06:10 +0200

    Separate ui from logic

--- app/credentials_dialog.py
+++ app/credentials_dialog.py
@@ -1,15 +1,13 @@
 from dotenv import load_dotenv, set_key, dotenv_values
 from modules import utils
+from ui.ui_credentials_dialog import Ui_CredentialsDialog
 from PyQt6 import QtWidgets, QtGui
 import os
 
-class CredentialsDialog(QtWidgets.QDialog):
+class CredentialsDialog(QtWidgets.QDialog, Ui_CredentialsDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.setWindowTitle('Credentials')
-        self.setFixedWidth(600)
-
-        layout = QtWidgets.QFormLayout(self)
+        self.setupUi(self)
 
         # Define the path to the .env file
         self.env_path = utils.get_env_path()
@@ -20,47 +18,17 @@ class CredentialsDialog(QtWidgets.QDialog):
         # Create line edits for credentials input
         self.setup_inputs()
 
-        # Create label for sections
-        ppmeCredentialsLabel = QtWidgets.QLabel('PlanningPME Credentials:')
-        msCredentialsLabel = QtWidgets.QLabel('Microsoft Azure Credentials:')
-        # Set font to bold
-        boldFont = QtGui.QFont()
-        boldFont.setBold(True)
-        ppmeCredentialsLabel.setFont(boldFont)
-        msCredentialsLabel.setFont(boldFont)
-        
-        # Add line edits to layout
-        layout.addRow(ppmeCredentialsLabel, QtWidgets.QLabel(''))  # Section title
-        layout.addRow('PPME Endpoint', self.ppmeEndpointEdit)
-        layout.addRow('PPME Appkey', self.ppmeAppkeyEdit)
-        layout.addRow('PPME Auth Token', self.ppmeAuthTokenEdit)
-
-        layout.addRow(msCredentialsLabel, QtWidgets.QLabel(''))  # Section title
-        layout.addRow('Client ID:', self.MSClientIdEdit)
-        layout.addRow('Client Secret:', self.MSClientSecretEdit)
-        layout.addRow('Tenant ID:', self.MSTenantIdEdit)
-
-        # Save button
-        self.saveButton = QtWidgets.QPushButton('Save', self)
         self.saveButton.clicked.connect(self.save_credentials)
-        layout.addRow(self.saveButton)
 
     def setup_inputs(self):
-        self.ppmeEndpointEdit = QtWidgets.QLineEdit(self.env_vars.get('PPME_ENDPOINT'))
-        self.ppmeAppkeyEdit = QtWidgets.QLineEdit(self.env_vars.get('PPME_APPKEY'))
-        self.ppmeAuthTokenEdit = QtWidgets.QLineEdit()
-        self.ppmeAuthTokenEdit.setEchoMode(QtWidgets.QLineEdit.EchoMode.Password)
+        self.ppmeEndpointEdit.setText(self.env_vars.get('PPME_ENDPOINT'))
+        self.ppmeAppkeyEdit.setText(self.env_vars.get('PPME_APPKEY'))
         if 'PPME_AUTH_TOKEN' in self.env_vars and self.env_vars['PPME_AUTH_TOKEN']:
             self.ppmeAuthTokenEdit.setPlaceholderText("[unchanged]")
-            self.ppmeAuthTokenEdit.setEchoMode(QtWidgets.QLineEdit.EchoMode.PasswordEchoOnEdit)
-
-        self.MSClientIdEdit = QtWidgets.QLineEdit(self.env_vars.get('MS_CLIENT_ID'))
-        self.MSClientSecretEdit = QtWidgets.QLineEdit()
-        self.MSClientSecretEdit.setEchoMode(QtWidgets.QLineEdit.EchoMode.Password)
+        self.MSClientIdEdit.setText(self.env_vars.get('MS_CLIENT_ID'))
         if 'MS_CLIENT_SECRET' in self.env_vars and self.env_vars['MS_CLIENT_SECRET']:
             self.MSClientSecretEdit.setPlaceholderText("[unchanged]")
-            self.MSClientSecretEdit.setEchoMode(QtWidgets.QLineEdit.EchoMode.PasswordEchoOnEdit)
-        self.MSTenantIdEdit = QtWidgets.QLineEdit(self.env_vars.get('MS_TENANT_ID'))
+        self.MSTenantIdEdit.setText(self.env_vars.get('MS_TENANT_ID'))
 
     def load_env_vars(self):
         load_dotenv(self.env_path)  # Load environment variables from the specified .env file
---
+++ ui/ui_credentials_dialog.py
@@ -0,0 +1,44 @@
+# ui_credentials_dialog.py
+from PyQt6 import QtWidgets, QtGui
+
+class Ui_CredentialsDialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setWindowTitle('Credentials')
+        Dialog.setFixedWidth(600)
+
+        layout = QtWidgets.QFormLayout(Dialog)
+
+        # Create labels for sections
+        self.ppmeCredentialsLabel = QtWidgets.QLabel('PlanningPME Credentials')
+        self.MSCredentialsLabel = QtWidgets.QLabel('Microsoft Azure Credentials')
+
+        # Set font to bold
+        boldFont = QtGui.QFont()
+        boldFont.setBold(True)
+        self.ppmeCredentialsLabel.setFont(boldFont)
+        self.MSCredentialsLabel.setFont(boldFont)
+
+        # Add labels and line edits to layout
+        layout.addRow(self.ppmeCredentialsLabel, QtWidgets.QLabel(''))  # Section title
+        self.ppmeEndpointEdit = QtWidgets.QLineEdit()
+        self.ppmeAppkeyEdit = QtWidgets.QLineEdit()
+        self.ppmeAuthTokenEdit = QtWidgets.QLineEdit()
+        self.ppmeAuthTokenEdit.setEchoMode(QtWidgets.QLineEdit.EchoMode.Password)
+
+        layout.addRow('PPME Endpoint', self.ppmeEndpointEdit)
+        layout.addRow('PPME Appkey', self.ppmeAppkeyEdit)
+        layout.addRow('PPME Auth Token', self.ppmeAuthTokenEdit)
+
+        layout.addRow(self.MSCredentialsLabel, QtWidgets.QLabel(''))  # Section title
+        self.MSClientIdEdit = QtWidgets.QLineEdit()
+        self.MSClientSecretEdit = QtWidgets.QLineEdit()
+        self.MSClientSecretEdit.setEchoMode(QtWidgets.QLineEdit.EchoMode.Password)
+        self.MSTenantIdEdit = QtWidgets.QLineEdit()
+
+        layout.addRow('Client ID:', self.MSClientIdEdit)
+        layout.addRow('Client Secret:', self.MSClientSecretEdit)
+        layout.addRow('Tenant ID:', self.MSTenantIdEdit)
+
+        # Save button
+        self.saveButton = QtWidgets.QPushButton('Save', Dialog)
+        layout.addRow(self.saveButton)
