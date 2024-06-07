dca43c5e71fc1f0bde7a922014a19dea5fb6d32e
16205 <gabriel.lohest@gmail.com>
2024-06-07 09:58:02 +0200

    Remove PPME Endpoint, Client ID and Tenant ID from credentials dialog

--- ui/ui_credentials_dialog.py
+++ ui/ui_credentials_dialog.py
@@ -8,36 +8,27 @@ class Ui_CredentialsDialog(object):
 
         layout = QtWidgets.QFormLayout(Dialog)
 
-        # Create labels for sections
-        self.ppmeCredentialsLabel = QtWidgets.QLabel('PlanningPME Credentials')
-        self.MSCredentialsLabel = QtWidgets.QLabel('Microsoft Azure Credentials')
-
         # Set font to bold
         boldFont = QtGui.QFont()
         boldFont.setBold(True)
-        self.ppmeCredentialsLabel.setFont(boldFont)
-        self.MSCredentialsLabel.setFont(boldFont)
 
-        # Add labels and line edits to layout
-        self.ppmeEndpointEdit = QtWidgets.QLineEdit()
+        # PlanningPME Credentials Section
+        self.ppmeCredentialsLabel = QtWidgets.QLabel('PlanningPME Credentials')
+        self.ppmeCredentialsLabel.setFont(boldFont)
         self.ppmeAppkeyEdit = QtWidgets.QLineEdit()
         self.ppmeAuthTokenEdit = QtWidgets.QLineEdit()
         self.ppmeAuthTokenEdit.setEchoMode(QtWidgets.QLineEdit.EchoMode.Password)
-
         layout.addRow(self.ppmeCredentialsLabel, QtWidgets.QLabel(''))  # Section title
-        layout.addRow('PPME Endpoint', self.ppmeEndpointEdit)
         layout.addRow('PPME Appkey', self.ppmeAppkeyEdit)
         layout.addRow('PPME Auth Token', self.ppmeAuthTokenEdit)
 
-        self.MSClientIdEdit = QtWidgets.QLineEdit()
+        # Microsoft Azure Credentials Section
+        self.MSCredentialsLabel = QtWidgets.QLabel('Microsoft Azure Credentials')
+        self.MSCredentialsLabel.setFont(boldFont)
         self.MSClientSecretEdit = QtWidgets.QLineEdit()
         self.MSClientSecretEdit.setEchoMode(QtWidgets.QLineEdit.EchoMode.Password)
-        self.MSTenantIdEdit = QtWidgets.QLineEdit()
-
         layout.addRow(self.MSCredentialsLabel, QtWidgets.QLabel(''))  # Section title
-        layout.addRow('Client ID', self.MSClientIdEdit)
         layout.addRow('Client Secret', self.MSClientSecretEdit)
-        layout.addRow('Tenant ID', self.MSTenantIdEdit)
 
         # Save button
         self.saveButton = QtWidgets.QPushButton('Save', Dialog)
